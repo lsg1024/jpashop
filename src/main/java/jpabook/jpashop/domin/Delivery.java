a988b439edddff1392aa98922b2db4fbe5d4c194
lsg1024 <zks145@naver.com>
2023-11-09 14:31:58 +0900

    feat : 주문 관련 도메인 entity 작성 및 db 관계 작성

---
+++ src/main/java/jpabook/jpashop/domin/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domin;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // STRING and ORDINAL 두가지 상태가 존재하는데 ORDINAL 경우에는 숫자로 값을 표현해 중간에 새로운 값이 생기면 기존 값이 밀리는 현상이 생겨 오류를 유발하기에 절대 사용하면 안된다
+    private DeliveryStatus status; // READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domin/DeliveryStatus.java
@@ -0,0 +1,4 @@
+package jpabook.jpashop.domin;
+
+public enum DeliveryStatus {
+}
