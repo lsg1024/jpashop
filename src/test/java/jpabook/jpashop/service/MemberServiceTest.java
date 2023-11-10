35441cec2bb0ac90a75356161b4f5caa81f2903e
lsg1024 <zks145@naver.com>
2023-11-10 20:52:00 +0900

    feat : 회원 도메인 TEST 코드 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domin.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+//    @Rollback(value = false) // 실제 DB에 들어가는지 확인
+    public void createID() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+//        em.flush(); // 실제 쿼리 코드를 보여줌
+        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+    @Test()
+    public void sameID_Check() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        }, "예외가 발생해야 한다");
+    }
+}
